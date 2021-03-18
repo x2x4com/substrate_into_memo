e8bc976b04556368b0597593cc34bdc98e2ef99e
Jacky <x2x4com@gmail.com>
2021-03-18 20:03:12 +0800

    remove target

--- code/l3/echo-server/target/debug/deps/echo_server.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Data/Git/github/x2x4/substrate_into_memo/code/l3/echo-server/target/debug/deps/echo_server: src/main.rs
-
-/Volumes/Data/Git/github/x2x4/substrate_into_memo/code/l3/echo-server/target/debug/deps/echo_server.d: src/main.rs
-
-src/main.rs:
--- code/l3/echo-server/target/debug/echo-server.d
+++
@@ -1,1 +0,0 @@
-/Volumes/Data/Git/github/x2x4/substrate_into_memo/code/l3/echo-server/target/debug/echo-server: /Volumes/Data/Git/github/x2x4/substrate_into_memo/code/l3/echo-server/src/main.rs
